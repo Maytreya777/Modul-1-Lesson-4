303879e6f383b5c21be0c269c857264efcc39cb4
Maytreya777 <maytreya777@gmail.com>
2024-07-08 16:02:20 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = ('вселенная любит нас')
+print(len(my_string)) # 19
+print(my_string.upper()) # ВСЕЛЕННАЯ ЛЮБИТ НАС
+print(my_string.lower()) # вселенная любит нас
+print(my_string.replace(' ','')) # вселеннаялюбитнас
+print(my_string[0]) # в
+print(my_string[-1]) # c
